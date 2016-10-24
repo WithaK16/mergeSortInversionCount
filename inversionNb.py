42f8748d8b90afac5eabc0560e21ccc507859202
karl <karlbertoli16@gmail.com>
2016-10-24 17:52:42 +0800

    [UPDATE] Working but maybe not most optimal implementation :)

---
+++ inversionNb.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 15:47:07 2016
+
+@author: karl
+"""
+import pandas as pd
+import numpy as np
+
+
+#Reading data with panda
+intArray = np.array((pd.read_csv("intArray.txt", header=None)))
+intArrayList = [i[0] for i in intArray]
+
+def merge(array1, array2):
+    
+    mergeArray = []
+    countInversion = 0
+    i = 0 # count for array 1
+    j = 0 # count for array 2
+
+    lenArray1 = len(array1)
+    lenArray2 = len(array2)
+    
+    while ((i < lenArray1) | (j < lenArray2)):
+        if (i == lenArray1):
+            mergeArray.append(array2[j])
+            j += 1
+        elif (j == lenArray2):
+            mergeArray.append(array1[i])
+            i += 1
+        else:
+            if (array2[j] < array1[i]):
+                mergeArray.append(array2[j])
+                countInversion += lenArray1-i
+                j += 1
+            else:
+                mergeArray.append(array1[i])
+                i+= 1
+    return mergeArray, countInversion
+
+def mergeSortCount(array):
+    countInversion = 0
+    if len(array) == 1:
+        return array, 0
+    else:
+        array1, countInversion1 = mergeSortCount(array[:len(array)/2])
+        array2, countInversion2 = mergeSortCount(array[len(array)/2:])
+        arraySorted, countInversionSorted = merge(array1, array2)
+        countInversion = countInversion1 + countInversion2 + countInversionSorted
+        return arraySorted, countInversion
+        
+arraySorted, numberOfInversion = mergeSortCount(intArrayList)
